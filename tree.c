db4ca8657e94006a75eeae8a3232f08afe9dc303
Betty <bettythomas.mary@gmail.com>
2018-02-26 11:16:05 +0530

    Commit successful

---
+++ tree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int x,y
+printf("Enter the value of x\n");
+scanf("%d",&x);
+printf("Enter the value of y\n");
+scanf("%d",&y);
+for(i=0;i<x;i++)
+printf("%d",i);
+printf("\n");
+for(i=0;i<y;i++)
+printf("%d",i)
+}
